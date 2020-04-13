34e7c33a3f17e72eea8b071ef7f89399a157f630
cihataltuntas <btabcdef>
2020-04-13 10:26:05 +0300

    chore: .mvn folder is added & cors setup

--- src/main/java/com/ericsson/poccontractmanagement/PocContractManagementApplication.java
+++ src/main/java/com/ericsson/poccontractmanagement/PocContractManagementApplication.java
@@ -3,6 +3,14 @@ package com.ericsson.poccontractmanagement;
 import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 @EnableProcessApplication
@@ -11,5 +19,35 @@ public class PocContractManagementApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PocContractManagementApplication.class, args);
 	}
+	
+	/*
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedMethods("POST", "GET", "PUT", "DELETE", "HEAD")
+						.allowedOrigins("*")
+						.allowCredentials(false);
+			}
+		};
+	}
+	*/
+	
+	@Bean
+    public FilterRegistrationBean processCorsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+
+        FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+        bean.setOrder(0);
+        return bean;
+    }
 
 }
